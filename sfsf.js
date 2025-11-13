12628d920ab17a9026bd6f768364c7474797594a
Vishara Srinadi <visharasrinadi@gmail.com>
2025-11-13 23:58:30 +0530

    Update sfsf.js
    updated dashboard

--- sfsf.js
+++ sfsf.js
@@ -19,8 +19,6 @@ const HomePage = () => {
         description: "Library will be open from 7 AM to 9 PM during exam week.",
       },
     ];
-    setNotices(sampleNotices);
-  }, []);
 
   return (
     <div className="min-h-screen bg-gray-100 p-6">
